d67535067f459ec54402eeba6a72114142202f83
MartinsPlume <56507599+MartinsPlume@users.noreply.github.com>
2020-05-10 16:19:29 +0300

    Formatted Contracts

--- src/authorization/Contracts.js
+++ src/authorization/Contracts.js
@@ -1,7 +1,13 @@
-
 // authorization
-export const EDzControlLogin="https://localhost:5001/api/v1/login"
-export const EDzControlRegister="https://localhost:5001/api/v1/Users"
+const EDzControlLogin="https://localhost:5001/api/v1/login"
+const EDzControlRegister="https://localhost:5001/api/v1/Users"
 
 // Exercises
-export const exercises = 'https://localhost:5001/api/Exercises'
+const EDzControlExercises = 'https://localhost:5001/api/Exercises'
+
+
+export const WebApiRequests = {
+    EDzControlLogin,
+    EDzControlRegister,
+    EDzControlExercises
+}
