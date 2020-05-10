b7356b5a0e593cac5ffbf01af6d09aede9756e55
MartinsPlume <56507599+MartinsPlume@users.noreply.github.com>
2020-05-10 16:19:06 +0300

    Added string resources

--- src/res/strings.js
+++ src/res/strings.js
@@ -1,4 +1,80 @@
-// Login and register strings
-export const WelcomeText = 'Welcome'
-export const LogOffText = ' Log Off'
-export const NewExerciseText = 'New Exercise'
+// Label strings
+const LogOffText = ' Log Off'
+const CloseText = 'Close'
+const CloseTextWithSpaceForIcon = ' Close'
+const ExerciseText = 'Exercise'
+const NewTextWithSpaceForIcon = ' New'
+const EditTextWithSpaceForIcon = ' Edit'
+const AddExerciseText = 'Add Exercise'
+const ClearText = 'Clear'
+const SaveText = 'Save'
+const DeleteText = 'Delete'
+const ShortDescriptionText = 'Short description of Your exercise'
+const DescriptionText = 'Full description of the exercise'
+const OkText = 'Ok!'
+
+// Hint strings
+const ExerciseTextHint = 'Enter the name of Your exercise'
+const ExerciseShortDescriptionHint = 'Enter in one or two sentences short description of the given exercise'
+const ExerciseDescriptionHint = 'Enter full description of the given exercise'
+
+export const Strings = {
+    // Export labels
+    LogOffText,
+    CloseText,
+    CloseTextWithSpaceForIcon,
+    ExerciseText,
+    NewTextWithSpaceForIcon,
+    EditTextWithSpaceForIcon,
+    AddExerciseText,
+    ClearText,
+    SaveText,
+    DeleteText,
+    ShortDescriptionText,
+    DescriptionText,
+    OkText,
+
+    // Export hints
+    ExerciseTextHint,
+    ExerciseShortDescriptionHint,
+    ExerciseDescriptionHint
+}
+
+// ExerciseActionSwitch strings
+const ActionSwitchNewExercise = 'NewExercise'
+const ActionSwitchEditExercise = 'EditExercise'
+const ActionSwitchIdle = 'Idle'
+
+export const ActionSwitchStrings = {
+    ActionSwitchNewExercise,
+    ActionSwitchEditExercise,
+    ActionSwitchIdle
+}
+
+// Exercise Handle change switch strings
+const ExerciseName = 'ExerciseName'
+const ShortDescription = 'ShortDescription'
+const Description = 'Description'
+
+export const ExerciseChangeSwitchStrings = {
+    ExerciseName,
+    ShortDescription,
+    Description
+}
+
+// Modal status
+const ModalIdle = 'Idle'
+const ModalUpdated = 'Updated'
+const ModalAdded = 'Added'
+const ModalDeleted = 'Deleted'
+
+export const ModalStatusStrings = {
+    ModalIdle,
+    ModalUpdated,
+    ModalAdded,
+    ModalDeleted
+}
+
+// export const ModalMessageStrings = {
+
+// }
