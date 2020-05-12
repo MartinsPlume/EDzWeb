20a3bf45178b63fd5cf223bee6ac94842d101d71
MartinsPlume <56507599+MartinsPlume@users.noreply.github.com>
2020-05-12 10:14:03 +0300

    Updated WebApiRequests

--- src/authorization/Contracts.js
+++ src/authorization/Contracts.js
@@ -3,10 +3,10 @@ const EDzControlLogin="https://localhost:5001/api/v1/login"
 const EDzControlRegister="https://localhost:5001/api/v1/Users"
 
 // Exercises
-const EDzControlExercises = 'https://localhost:5001/api/TeacherExercises'
+const EDzControlExercises = 'https://localhost:5001/api/Exercises'
 
 // TeacherAssignments
-const EDzControlTeacherAssignments = 'https://localhost:5001/api/TeacherAssignment'
+const EDzControlTeacherAssignments = 'https://localhost:5001/api/Assignment'
 
 // Get students
 const EDzControlGetStudents = 'https://localhost:5001/api/v1/students'
